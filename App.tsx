01988ad309411edc16df563c961c5f173671cb61
Vinícius Vieira <vinicius.vieira.ime@gmail.com>
2020-08-20 12:30:46 -0300

    Included CPF validation

--- App.tsx
+++ App.tsx
@@ -1,28 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+//import { SafeAreaProvider } from 'react-native-safe-area-context';
+import App from './src/App'
+import { AppLoading } from 'expo'
+import { useFonts } from '@use-expo/font';
+
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
-import { withAuthenticator } from 'aws-amplify-react-native'
 
-Amplify.configure(config)
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+//import Navigation from './navigation';
 
-export default function App() {
+Amplify.configure(config)
+
+function AppWithNavigationContainer() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
+  let [fontsLoaded] = useFonts({
+    'SourceSansPro-Regular': require('./src/assets/fonts/SourceSansPro-Regular.ttf'),
+    'SourceSansPro-SemiBold': require('./src/assets/fonts/SourceSansPro-SemiBold.ttf'),
+  });
+
+  if (!isLoadingComplete || !fontsLoaded) {
+    return <AppLoading />
   } else {
     return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+    );
+  /*  return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
-    );
+    );*/
   }
 }
+
+export default AppWithNavigationContainer
